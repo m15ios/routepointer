b43c8ad4146048477769d30bc894b97703af9efc
Mike Petrenko <mpetrenko.m15ios@gmail.com>
2022-02-05 16:36:44 +0300

    add map and place on full sreen

--- routepointer/ViewController.swift
+++ routepointer/ViewController.swift
@@ -6,15 +6,41 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        // set TAMIC
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .gray
+        setConstraints()
     }
 
 
 }
 
+extension ViewController {
+    
+    func setConstraints(){
+        // place our mapView
+        view.addSubview(mapView)
+        // set constraints to full-screen
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        
+    }
+    
+}
